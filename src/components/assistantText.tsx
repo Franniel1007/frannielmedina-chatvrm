ae01e157399f71536992c7e2d75b93d769686e65
Franniel1007 <126989053+Franniel1007@users.noreply.github.com>
2025-09-20 21:18:42 -0500

    Update assistantText.tsx

--- src/components/assistantText.tsx
+++ src/components/assistantText.tsx
@@ -1,10 +1,27 @@
-export const AssistantText = ({ message }: { message: string }) => {
+import { Montserrat } from "next/font/google";
+import { M_PLUS_2 } from "next/font/google";
+
+const m_plus_2 = M_PLUS_2({
+  variable: "--font-m-plus-2",
+  display: "swap",
+  preload: false,
+});
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  display: "swap",
+  subsets: ["latin"],
+});
+
+// --- AÑADIR: La propiedad characterName ---
+export const AssistantText = ({ message, characterName }: { message: string, characterName: string }) => {
   return (
     <div className="absolute bottom-0 left-0 mb-104  w-full">
       <div className="mx-auto max-w-4xl w-full p-16">
         <div className="bg-white rounded-8">
           <div className="px-24 py-8 bg-secondary rounded-t-8 text-white font-Montserrat font-bold tracking-wider">
-            CHARACTER
+            {/* --- UTILIZAR LA PROPIEDAD characterName --- */}
+            {characterName.toUpperCase()}
           </div>
           <div className="px-24 py-16">
             <div className="text-secondary typography-16 font-M_PLUS_2 font-bold">
